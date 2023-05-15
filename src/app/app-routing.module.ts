d85aba891b70e63e82df3e4e4897df431932d980
vladlen-li-idf <vladlen.li@solva.kz>
2023-05-15 15:31:58 +0600

    feat: create project base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {NotificationComponent} from './notification/notification.component';
+import {SupportComponent} from './support/support.component';
+import {CoreComponent} from './core/core.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'notification', component: NotificationComponent},
+  {path: 'support', component: SupportComponent},
+  {path: 'core', component: CoreComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
